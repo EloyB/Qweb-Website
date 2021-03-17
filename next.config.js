7d0731e6c984b124ec39b4eabde0a102497716dc
EloyB <eloyboone@hotmail.com>
2021-03-17 20:03:20 +0100

    added footer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
     MAIL_URL:
-      "https://prod-61.westeurope.logic.azure.com:443/workflows/81b128fb062243518c2971242f211a0c/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=d__qChh5wtEOQKVcgBv4M91fXLeALnm4hHoOQarzW9Y",
+      "https://prod-28.ukwest.logic.azure.com:443/workflows/ec9b1de489dc4985a783783df55a52d6/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Z0RbQea8GkypcqdblaCAcQESm9dn6RMfId3vRAWRKuc",
   },
 };
