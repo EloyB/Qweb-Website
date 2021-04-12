8ac44d7dc43488fa753f3ac2a732d3003fbbc97a
EloyB <eloyboone@hotmail.com>
2021-04-12 22:18:27 +0200

    multicolor

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,30 @@ module.exports = {
       fontFamily: {
         poppins: "Poppins, sans-serif",
       },
+
+      //BLUE
       colors: {
-        primary: "#61C79F",
-        secondary: "#183540",
-        "secondary-light": "#26434E",
-        "primary-dark": "#56B28E",
+        primary: "#1E90FF",
+        secondary: "#222237",
+        "secondary-light": "#363653",
+        "primary-dark": "#0081FF",
       },
+
+      // GREEN
+      // colors: {
+      //   primary: "#61C79F",
+      //   secondary: "#183540",
+      //   "secondary-light": "#26434E",
+      //   "primary-dark": "#56B28E",
+      // },
+
+      // ORANGE
+      // colors: {
+      //   primary: "#FDA085",
+      //   secondary: "#4B5558",
+      //   "secondary-light": "#5C676A",
+      //   "primary-dark": "#ED8365",
+      // },
     },
   },
   variants: {
